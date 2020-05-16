4ab366612757424301f36e1efb5f4b118b812166
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-16 12:25:57 -0400

    added a state object which grabs the onChange value

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,13 +2,15 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
+  state = { term: ''};
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div>
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input type="text" onChange={(e) => this.setState({term : e.target.value})}/>
           </div>
         </form>
       </div>
