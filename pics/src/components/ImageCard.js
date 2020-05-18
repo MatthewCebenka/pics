38a344f80e24cc564151cc0202ad8e85669bfae1
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-18 19:41:59 -0400

    passed the api props down to the imagecard component

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 class ImageCard extends React.Component {
     render() {
+        const {description, urls } = this.props.image;
+
         return (
             <div>
-                <h1>Hello there!</h1>
+                <img 
+                alt={description}
+                src={urls.regular}
+                />
             </div>
         )
     }
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,13 +3,12 @@ import ImageCard from "./ImageCard";
 import "./ImageList.css";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
   console.log(images);
   return (
     <div className="image-list">
-      <ImageCard />
       <div>{images}</div>
     </div>
   );
