623dd841f1d2928af25c3a714e959ce171f681a0
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-18 08:33:38 -0400

    added a param and header to our get request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-        params: { query: term},
-        headers: {
-            Authorization: 'Client-ID Ws1Pp9TTWrv9wjsRtci1xcNYeybWAnjRG8NzlHIjQDk'
-        }
-        });
-    }
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID Ws1Pp9TTWrv9wjsRtci1xcNYeybWAnjRG8NzlHIjQDk",
+      },
+    });
+  }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+        <SearchBar onSearchSubmit={this.onSearchSubmit} />
       </div>
     );
   }
